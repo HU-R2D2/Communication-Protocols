9bcaead6b4b1830403035109a069c333620f0d13
Norazan <thijs.hendrickx@online.nl>
2016-05-13 11:54:15 +0200

    Added TransportProtocol header

---
+++ source/include/TransportProtocol.hpp
@@ -0,0 +1,22 @@
+class TransportProtocol{
+
+public:
+	TransportProtocol();
+	~TransportProtocol();
+
+	virtual void data_write(uint8_t[] data) = 0;
+
+	virtual uint8_t[] data_read() = 0;
+
+	virtual void connect() = 0;
+
+	virtual void disconnect() = 0;
+
+	virtual void flush() = 0;
+
+	virtual bool is_open() = 0;
+
+private:
+	std::queue<uint8_t> send_buffer;
+	std::queue<uint8_t> receive_buffer;
+}
