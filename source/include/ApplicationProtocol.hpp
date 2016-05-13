b738bc9a489ce83217c99f80aa53e8ec28f1364a
Job Verhaar <job.verhaar@student.hu.nl>
2016-05-13 12:12:58 +0200

    implemented applicationprotocol

---
+++ source/include/ApplicationProtocol.hpp
@@ -0,0 +1,12 @@
+#ifndef _APPLICATION_HPP
+#define _APPLICATION_HPP
+#include "TransportProtocol.hpp"
+class ApplicationProtocol
+{
+public:
+    ApplicationProtocol();
+    ApplicationProtocol(TransportProtocol t);
+private:
+    TransportProtocol transport;
+};
+#endif
