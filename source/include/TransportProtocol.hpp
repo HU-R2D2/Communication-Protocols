043771e5b15aae31cec248b8863bd96ea9610d90
Norazan <thijs.hendrickx@online.nl>
2016-06-13 12:23:04 +0200

    Added listener pattern to UART, and implemented in-object thread creation for the run method.

--- source/include/TransportProtocol.hpp
+++ source/include/TransportProtocol.hpp
@@ -2,6 +2,7 @@
 #define _TRANSPORTPROTOCOL_HPP
 
 #include <cstdint>
+#include "TransportListener.hpp"
 
 class TransportProtocol{
 
@@ -18,6 +19,18 @@ public:
 	virtual void flush() = 0;
 
 	virtual bool is_open() = 0;
+
+	virtual void set_listener(TransportListener * t) = 0;
+
+	virtual void remove_listener(TransportListener * t) = 0;
+
+protected:
+
+	std::vector<TransportListener *> transportListeners;
+	
+	std::queue<uint8_t> send_buffer;
+	
+	std::queue<uint8_t> receive_buffer;
 };
 
 #endif
