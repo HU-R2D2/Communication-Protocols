1c12c9c61e9924b5b53326be7711f3868453309b
Job Verhaar <job.verhaar@student.hu.nl>
2016-06-14 15:15:08 +0200

    created uarttest application protocol, this protocol is created for testing purpose only

--- source/src/Main.cpp
+++ source/src/Main.cpp
@@ -1,5 +1,9 @@
 #include "../include/UART.hpp"
+#include "../include/UartTester.hpp"
 #include <iostream>
 int main(){
     std::cout << "Hello!" << std::endl;
+    UART u(COM4);
+    UartTester t(&u, false);
+    t.sayHi();
 }
